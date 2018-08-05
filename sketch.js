7a1ebcd839c07fb8ea662b544736fede925d6c03
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-05 13:46:34 -0400

    Adding slider to change circle size

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 var bgcol;
 var button;
+var slider;
 
 function setup(){
   createCanvas(200, 200);
 	bgcolor = color(200);
-	button = createButton("go go go");
+	createP("");  // to moe button below
+	button = createButton("Go");
 	button.mousePressed(changeColor);
+	slider = createSlider(10, 100, 40);
 }
 
 function draw(){
   background(bgcolor);
 	fill(255, 0, 0);
-  rect(10, 10, 20, 20);
+  ellipse(50, 50, slider.value(), slider.value());
 }
 
 function changeColor(){
