f0a7a0704250ef1d6a2bc40496f67de5b8f22fed
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-05 12:28:38 -0400

    create element in p5js
    create p on mousePressed()

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
 function setup(){
   createCanvas(600, 400);
+	createElement('h1', "My favorite text below");
 }
 
 function draw(){
   background(128);
   rect(100, 100, 50, 50);
+}
+
+function mousePressed(){
+	createP("this is my best idea!");
 }
