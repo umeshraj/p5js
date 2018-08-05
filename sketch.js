87aa079a85ef9d3709a370c13edf08a21d85ec23
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-05 12:40:42 -0400

    Absolute position of canvas using vars
    Using html to change content

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
+var myCanvas;
+var myH1;
 function setup(){
-  createCanvas(600, 400);
-	createElement('h1', "My favorite text below");
+  myCanvas = createCanvas(200, 200);
+	myCanvas.position(0, 300);
+	myH1 = createElement('h1', "Waiting....");
 }
 
 function draw(){
   background(128);
-  rect(100, 100, 50, 50);
+	fill(255, 0, 0);
+  rect(10, 10, 20, 20);
 }
 
 function mousePressed(){
+	myH1.html("I will show you some text")
 	createP("this is my best idea!");
 }
