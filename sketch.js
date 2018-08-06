4b1fba9fe60bdddeed088f9e7f86d7c4f39eaee1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-06 08:27:54 -0400

    Changing styles from JS

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
+let para;
+var button;
 function setup(){
+  createCanvas(100, 100);
+  background(0);
+
+  para = select("#unicorn");
+  para.mouseOver(changeBackground);
+  button = select("#button");
+  button.mousePressed(canvasBkgnd);
 }
 
-function draw(){
-  
+function canvasBkgnd(){
+  background(random(255));
+}
+function changeBackground(){
+  para.style('background-color', 'green');
 }
