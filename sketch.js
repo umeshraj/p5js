bd346e60bc7af87c912f75883b3479d9726346f1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-06 07:29:05 -0400

    tag, class, id using css selectors

--- sketch.js
+++ sketch.js
@@ -1,21 +1,6 @@
-var textBox;
-var slider;
-var para;
-
 function setup(){
-  noCanvas();
-  para = createP('starting text');
-  textBox = createInput('enter text');
-  slider = createSlider(10, 64, 16);
-
-  textBox.input(doSomething);
-  slider.input(updateSize);
 }
 
-function doSomething(){
-  para.html(textBox.value());
-}
-
-function updateSize(){
-  para.style("font-size", slider.value() + "pt");
+function draw(){
+  
 }
