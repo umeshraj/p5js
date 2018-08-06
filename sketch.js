ca61226e2c04cee2f6d2719d98a94b390e57655b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-06 07:09:37 -0400

    Very simple text/input example

--- sketch.js
+++ sketch.js
@@ -1,61 +1,16 @@
-var bgcol;
-var button;
+var textBox;
 var slider;
-var nameInput;
-var nameP;
-var text2;
+var para;
 
 function setup(){
-  createCanvas(200, 200);
-	bgcolor = color(200);
-	nameP = createP("Your name");  // to moe button below
+  noCanvas();
+  para = createP('starting text');
+  textBox = createInput('enter text');
+  slider = createSlider(10, 64, 16);
 
-	button = createButton("Go");
-	button.mousePressed(changeColor);
-	slider = createSlider(10, 100, 40);
-	text2 = createP("This is another paragraph");
-
-	text2.mouseOver(changeStyle);
-	text2.mouseOut(revertStyle);
-
-	nameInput = createInput("type your name");
-	nameInput.input(updateText);
-
-	nameP.mouseOver(overpara);
-	nameP.mouseOut(outpara);
-
+  textBox.changed(doSomething);
 }
 
-function draw(){
-  background(bgcolor);
-	fill(255, 0, 0);
-  ellipse(50, 50, slider.value(), slider.value());
-	text(nameInput.value(), 75, 75);
-	// nameP.html(input.value());
-}
-
-function changeStyle(){
-	text2.style("background-color", "pink");
-	text2.style("color", "black");
-}
-
-function revertStyle(){
-	text2.style("background-color", "purple");
-	text2.style("color", "white");
-}
-
-function updateText(){
-	nameP.html(nameInput.value());
-}
-
-function overpara(){
-	nameP.html('your mouse is over me');
-}
-
-function outpara(){
-	nameP.html('your mouse is out of para');
-}
-
-function changeColor(){
-	bgcolor = color(random(1, 255));
+function doSomething(){
+  para.html(textBox.value());
 }
