a6d813970fd183167beba50130c2b0846596549f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-05 20:09:22 -0400

    Adding mouseover and mouseout

--- sketch.js
+++ sketch.js
@@ -8,11 +8,17 @@ function setup(){
   createCanvas(200, 200);
 	bgcolor = color(200);
 	nameP = createP("Your name");  // to moe button below
+
 	button = createButton("Go");
 	button.mousePressed(changeColor);
 	slider = createSlider(10, 100, 40);
 	createP("");
 	input = createInput("type your name");
+
+	nameP.mouseOver(overpara);
+	nameP.mouseOut(outpara);
+
+
 }
 
 function draw(){
@@ -20,7 +26,15 @@ function draw(){
 	fill(255, 0, 0);
   ellipse(50, 50, slider.value(), slider.value());
 	text(input.value(), 75, 75);
-	nameP.html(input.value());
+	// nameP.html(input.value());
+}
+
+function overpara(){
+	nameP.html('your mouse is over me');
+}
+
+function outpara(){
+	nameP.html('your mouse is out of para');
 }
 
 function changeColor(){
