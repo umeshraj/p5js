a0947f017a37555de67a7c013d660bcda2ae46be
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-06 14:52:02 -0400

    Using this. to assign callback to each element of array

--- sketch.js
+++ sketch.js
@@ -4,26 +4,27 @@ function setup(){
   createCanvas(100, 100);
   background(0);
 
-  para = select("#unicorn");
-  para.mouseOver(highlight);
-  para.mouseOut(unhighlight);
-  // para = selectAll(".rainbow");
-  // for (let p of para){
-  //   // p.mouseOver(changeBackground);
-  //   p.style("background-color", 'yellow');
-  // }
+  // para = select("#unicorn");
+  // para.mouseOver(highlight);
+  // para.mouseOut(unhighlight);
+  para = selectAll(".rainbow");
+  for (let p of para){
+    p.mouseOver(highlight);
+    p.mouseOut(unhighlight);
+    // p.style("background-color", 'yellow');
+  }
   button = select("#button");
   button.mousePressed(canvasBkgnd);
 }
 
 function highlight(){
-  para.style('padding', '16pt');
-  para.style('background-color', '#F0F');
+  this.style('padding', '16pt');
+  this.style('background-color', '#F0F');
 }
 
 function unhighlight(){
-  para.style('padding', '0pt');
-  para.style('background-color', 'white');
+  this.style('padding', '0pt');
+  this.style('background-color', 'white');
 }
 
 function canvasBkgnd(){
