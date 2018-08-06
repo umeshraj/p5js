d809787b559033d710852307b2cb60c46ea72b92
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-06 14:46:42 -0400

    Adding callback for one para

--- sketch.js
+++ sketch.js
@@ -4,14 +4,26 @@ function setup(){
   createCanvas(100, 100);
   background(0);
 
-  // para = select("#unicorn");
-  para = selectAll(".rainbow");
-  for (let p of para){
-    // p.mouseOver(changeBackground);
-    p.style("background-color", 'yellow');
-  }
+  para = select("#unicorn");
+  para.mouseOver(highlight);
+  para.mouseOut(unhighlight);
+  // para = selectAll(".rainbow");
+  // for (let p of para){
+  //   // p.mouseOver(changeBackground);
+  //   p.style("background-color", 'yellow');
+  // }
   button = select("#button");
   button.mousePressed(canvasBkgnd);
+}
+
+function highlight(){
+  para.style('padding', '16pt');
+  para.style('background-color', '#F0F');
+}
+
+function unhighlight(){
+  para.style('padding', '0pt');
+  para.style('background-color', 'white');
 }
 
 function canvasBkgnd(){
