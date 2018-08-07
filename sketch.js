c0db59696acf960bbb5e1e6fd19de613d777edf1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-07 19:38:42 -0400

    Simple example of assigning parent

--- sketch.js
+++ sketch.js
@@ -1,35 +1,9 @@
-let para;
-var button;
 function setup(){
-  createCanvas(100, 100);
+  var canvas = createCanvas(400, 400);
+  canvas.parent("canvasp")
+}
+
+function draw(){
   background(0);
-
-  // para = select("#unicorn");
-  // para.mouseOver(highlight);
-  // para.mouseOut(unhighlight);
-  para = selectAll(".rainbow");
-  for (let p of para){
-    p.mouseOver(highlight);
-    p.mouseOut(unhighlight);
-    // p.style("background-color", 'yellow');
-  }
-  button = select("#button");
-  button.mousePressed(canvasBkgnd);
-}
-
-function highlight(){
-  this.style('padding', '16pt');
-  this.style('background-color', '#F0F');
-}
-
-function unhighlight(){
-  this.style('padding', '0pt');
-  this.style('background-color', 'white');
-}
-
-function canvasBkgnd(){
-  background(random(255));
-}
-function changeBackground(){
-  p.style('background-color', 'green');
+  ellipse(200, 200, random(100), random(100));
 }
