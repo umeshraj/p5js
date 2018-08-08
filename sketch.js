efc21f6529358bcc6507143e9c1e342fb0202ccc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 07:20:16 -0400

    Adding images as anchors using parent(this)
    Note that the images are links!

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
-var p;
+
 function setup(){
   noCanvas();
-  p = createP("This is a link: ");
-  p.style("background-color", "#CCC");
-  p.style("padding", '16px');
+  for (let idx=0; idx<5; idx++){
+    let p = createP("This is a link: ");
+    p.style("background-color", "#CCC");
+    p.style("padding", '16px');
 
-  var a = createA("#", 'apples');
-  a.mousePressed(addPhoto);
-  a.parent(p);
+    var a = createA("#", 'apples');
+    a.mousePressed(addPhoto);
+    a.parent(p);
+  }
 }
 
 function addPhoto(){
   var img = createImg("images/flower.jpg");
   img.size(100, 100);
-  img.parent(p);
+  img.parent(this);
 }
