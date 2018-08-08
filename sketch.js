f12677cfb7fbc337929bc17a702eb94b9410c188
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 08:17:37 -0400

    Enable dragOver and dragLeave

--- sketch.js
+++ sketch.js
@@ -1,4 +1,17 @@
+var dropzone;
 function setup(){
   createCanvas(300, 300);
   background(0);
+
+  dropzone = select('#dropzone');
+  dropzone.dragOver(highlight);
+  dropzone.dragLeave(unhighlight);
+}
+
+function highlight(){
+  dropzone.style('background-color', 'lightgray');
+}
+
+function unhighlight(){
+  dropzone.style('background-color', 'white');
 }
