3121d36efb9bba66286b2019bcc27e83a974fa25
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 08:10:32 -0400

    Initial set up of screen to drop files

--- sketch.js
+++ sketch.js
@@ -1,31 +1,4 @@
-var images=[];
-
 function setup(){
-  noCanvas();
-  for (let idx=0; idx<5; idx++){
-    let p = createP("This is a link: ");
-    p.style("background-color", "#CCC");
-    p.style("padding", '16px');
-
-    var a = createA("#", 'apples');
-    a.mousePressed(addPhoto);
-    a.parent(p);
-  }
-
-  let button = select("#clear");
-  button.mousePressed(clearStuff);
-}
-
-function clearStuff(){
-  for (let im of images){
-    im.remove();
-  }
-}
-
-function addPhoto(){
-  var img = createImg("images/flower.jpg");
-  img.size(100, 100);
-  img.parent(this.parent());
-  // backup the images
-  images.push(img);
+  createCanvas(300, 300);
+  background(0);
 }
