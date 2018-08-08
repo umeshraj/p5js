f610bfb4e571d5525d6ce3ccfcacdf864d600bac
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 08:31:43 -0400

    Dropping an image onto a box

--- sketch.js
+++ sketch.js
@@ -6,6 +6,13 @@ function setup(){
   dropzone = select('#dropzone');
   dropzone.dragOver(highlight);
   dropzone.dragLeave(unhighlight);
+  // dropzone.drop(gotFile);
+}
+
+function gotFile(file){
+  createP(file.name + " " + file.size);
+  var img = createImg(file.data);
+  img.size(100, 100);
 }
 
 function highlight(){
