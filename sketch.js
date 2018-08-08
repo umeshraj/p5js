c5786f6cf680f5de01336608a0b42b8714b570f1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-07 20:10:40 -0400

    Dynamically change classes using this.class() and this.removeClass()

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 function setup(){
   createCanvas()
-  for (let idx=0; idx<100; idx++){
-    let p = createP('apples');
+  for (let idx=0; idx<5; idx++){
+    let p = createA('#', 'apples');
     let x = random(windowWidth);
     let y = random(windowHeight);
     p.position(x, y);
     p.class('apple');
   }
 
-  for (let idx=0; idx<100; idx++){
-    let p = createA('http://www.google.com', 'blueberries');
+  for (let idx=0; idx<5; idx++){
+    let p = createA('#', 'blueberry');
     let x = random(windowWidth);
     let y = random(windowHeight);
     p.position(x, y);
-    p.class('blueberries');
+    p.class('blueberry');
+    p.mousePressed(changeClass);
   }
 
+}
+
+function changeClass(){
+  this.removeClass('blueberry')
+  this.class('apple');
 }
