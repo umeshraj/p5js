340662d4895d1a491aa706f04c1f5f47e9b2f25f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 08:43:42 -0400

    copying example of image drop from p5js example
    Adblock was blocking image

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
-var dropzone;
-function setup(){
-  createCanvas(300, 300);
-  background(0);
 
-  dropzone = select('#dropzone');
-  dropzone.dragOver(highlight);
-  dropzone.dragLeave(unhighlight);
-  // dropzone.drop(gotFile);
+function setup() {
+  // create canvas
+  var c = createCanvas(710, 400);
+  background(100);
+  // Add an event for when a file is dropped onto the canvas
+  c.drop(gotFile);
 }
 
-function gotFile(file){
-  createP(file.name + " " + file.size);
-  var img = createImg(file.data);
-  img.size(100, 100);
+function draw() {
+  fill(255);
+  noStroke();
+  textSize(24);
+  textAlign(CENTER);
+  text('Drag an image file onto the canvas.', width/2, height/2);
+  noLoop();
 }
 
-function highlight(){
-  dropzone.style('background-color', 'lightgray');
-}
-
-function unhighlight(){
-  dropzone.style('background-color', 'white');
+function gotFile(file) {
+  // If it's an image file
+  if (file.type === 'image') {
+    // Create an image DOM element but don't show it
+    var img = createImg(file.data).hide();
+    // Draw the image onto the canvas
+    image(img, 0, 0, width, height);
+  } else {
+    println('Not an image file!');
+  }
 }
