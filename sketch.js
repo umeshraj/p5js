95b08d39a9e89f9025fa7c33eb89db43be274759
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 20:29:17 -0400

    Adding offset to make 100 dancing sliders

--- sketch.js
+++ sketch.js
@@ -4,14 +4,17 @@ let angle = 0;
 
 function setup() {
   noCanvas();
-  for(let idx=0; idx<10; idx++){
+  for(let idx=0; idx<100; idx++){
       sliders[idx] = createSlider(0, 255, 50);
   }
 }
 
 function draw() {
-  let x = map(sin(angle), -1, 1, 0, 255);
+  let offset = 0;
+  let x;
   for(let idx=0; idx<sliders.length; idx++){
+      x = map(sin(angle+offset), -1, 1, 0, 255);
+      offset += 0.0234;
       sliders[idx].value(x);
   }
   angle += 0.1;
