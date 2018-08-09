4a873c7fa70b026d5a917e834e36f707e4e134b0
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-09 07:50:04 -0400

    Using push/pop to rotate 1 rect and keep other static

--- sketch.js
+++ sketch.js
@@ -7,16 +7,18 @@ function setup() {
 
 function draw(){
   background(0);
+  push();
   translate(100, 100);
   rotate(angle);
 
   // rect1
   fill(255, 100, 50);
   rect(0, 0, 100, 50);
+  pop();
 
   //rect2
   fill(50, 100, 255);
-  rect(50, 50, 20, 20);
+  rect(300, 300, 100, 50);
 
   angle+=5;
 }
