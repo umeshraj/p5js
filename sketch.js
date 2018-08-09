cba9b44414f2700362c8f1d527480f93cd53641f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-08 20:26:43 -0400

    Multiple sliders moving in sync

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 
-let slider;
-let angle =0;
+let sliders = [];
+let angle = 0;
 
 function setup() {
-  createCanvas(300, 300);
-  slider = createSlider(0, 255, 50);
+  noCanvas();
+  for(let idx=0; idx<10; idx++){
+      sliders[idx] = createSlider(0, 255, 50);
+  }
 }
 
 function draw() {
   let x = map(sin(angle), -1, 1, 0, 255);
-  slider.value(x);
-  background(slider.value());
+  for(let idx=0; idx<sliders.length; idx++){
+      sliders[idx].value(x);
+  }
   angle += 0.1;
 }
