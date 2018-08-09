178460c42b261011f2ae87553f6603070ad5a02f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-09 07:38:00 -0400

    Adding translate and rotate

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
+let angle = 0;
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
 }
 
 function draw(){
   background(0);
-  // rect(50, 50, 100, 50);
+  translate(width/2, height/2);
+  rotate(angle);
 
-  // translate(50, 50);
-  translate(mouseX, mouseY);
-  rect(0, 0, 100, 50);
-
-  rect(50, 50, 100, 10);
+  stroke(255);
+  line(0, 0, 50, 50);
+  fill(255)
+  rect(50, 50, 100, 50);
+  angle+=1;
 }
