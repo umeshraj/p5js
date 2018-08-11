562310d99c229065fdd95ae0c4986009b7b75301
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-11 19:18:48 -0400

    Using circle object

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
+let circles = [];
+
 function setup(){
   createCanvas(640, 480);
   background(200);
 
   for(let idx=0; idx<25; idx++){
-    let x = random(width);
-    let y = random(height);
-    let r = 32;
-    noStroke();
-    fill(255, 0, 150, 100);
-    ellipse(x, y, 2*r, 2*r);
+    let circle = {
+      x: random(width),
+      y: random(height),
+      r: 32
+    };
+
+    circles.push(circle);
+  }
+
+  noStroke();
+  fill(255, 0, 100, 100);
+  for(let c of circles){
+    ellipse(c.x, c.y, 2*c.r, 2*c.r);
   }
 
 }
