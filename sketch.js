0f7dd6f62941c6c948cdbd55a3815a06a7b7f8fa
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-11 19:29:56 -0400

    Check if circle overlaps any other circle with dist()

--- sketch.js
+++ sketch.js
@@ -11,15 +11,31 @@ function setup(){
       r: 32
     };
 
-    circles.push(circle);
+    if (isCircleValid(circle)){
+      circles.push(circle);
+    }
   }
+  drawCircles();
+}
 
+function isCircleValid(circle){
+  isValid = true;
+  for (let c of circles){
+    let d = dist(circle.x, circle.y, c.x, c.y);
+    if (d < (circle.r + c.r)){
+      isValid = false;
+      break;
+    }
+  }
+  return isValid;
+}
+
+function drawCircles(){
   noStroke();
   fill(255, 0, 100, 100);
   for(let c of circles){
     ellipse(c.x, c.y, 2*c.r, 2*c.r);
   }
-
 }
 
 function draw(){
