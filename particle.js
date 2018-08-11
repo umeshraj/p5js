f5527fa55a5f08e09037a9e499c2a9e722837b13
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-11 09:53:28 -0400

    History of bouncing ball

--- particle.js
+++ particle.js
@@ -4,6 +4,7 @@ function Particle(x, y){
   this.x = x;
   this.y = y;
   this.yspeed = 0;
+  this.history = [];
 
   this.update = function(){
     this.y+=this.yspeed;
@@ -13,12 +14,22 @@ function Particle(x, y){
       this.y = height;
       this.yspeed *= -0.9;
     }
+
+    let v = createVector(this.x, this.y);
+    this.history.push(v);
   }
 
   this.show = function() {
       stroke(0);
       fill(0, 150);
-      ellipse(this.x, this.y, 24, 24);    
+      ellipse(this.x, this.y, 24, 24);
+
+      // showing history
+      for (let pos of this.history){
+        fill(128);
+        ellipse(pos.x, pos.y, 8, 8);
+      }
+
   }
 
 }
