80e66df2ca2f8de2ac1575d11393e7e408362ba8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-12 11:55:21 -0400

    Prim's algo for min-spanning tree

--- sketch.js
+++ sketch.js
@@ -11,8 +11,47 @@ function mousePressed(){
 }
 
 function draw(){
-  for (let v of vertices){
-    ellipse(v.x, v.y, 10, 10);
+  background(51);
+  let reached = [];
+  let unreached = [];
+  // move all vertices into unreached
+  for(let v of vertices){
+    unreached.push(v);
+  }
+  // pick the first unreached to start algo
+  reached.push(unreached[0]);
+  //removed selected from unreached
+  unreached.splice(0, 1);
+
+  // for each v in reached, find the closest v
+  while (unreached.length > 0){
+    let rIdx;
+    let uIdx;
+    // for every unreached
+    let maxDist = 100000;
+    for(let i=0; i<reached.length; i++){
+        for(let j=0; j<unreached.length; j++){
+          let r = reached[i];
+          let u = unreached[j];
+          let d = dist(r.x, r.y, u.x, u.y)
+          if (d < maxDist){
+            maxDist = d;
+            rIdx = i;
+            uIdx = j;
+          }
+        }
+    }
+    stroke(255);
+    line(reached[rIdx].x, reached[rIdx].y,
+      unreached[uIdx].x, unreached[uIdx].y)
+    reached.push(unreached[uIdx]);
+    unreached.splice(uIdx, 1);
   }
 
+
+  for (let v of vertices){
+    stroke(0);
+    fill(255);
+    ellipse(v.x, v.y, 10, 10);
+  }
 }
