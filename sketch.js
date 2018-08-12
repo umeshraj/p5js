b5a046ef59ff0785eacc1e92ae5ad7ce33161b57
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-12 11:24:22 -0400

    Add vertices by clicking

--- sketch.js
+++ sketch.js
@@ -1,45 +1,18 @@
-let circles = [];
+let vertices = [];
 
 function setup(){
   createCanvas(640, 480);
   background(200);
-
-  let protection = 0;
-  while ((circles.length < 250) && (protection<10000)){
-    let circle = {
-      x: random(width),
-      y: random(height),
-      r: random(10, 40)
-    };
-
-    if (isCircleValid(circle)){
-      circles.push(circle);
-    }
-    protection++;
-  }
-  drawCircles();
 }
 
-function isCircleValid(circle){
-  isValid = true;
-  for (let c of circles){
-    let d = dist(circle.x, circle.y, c.x, c.y);
-    if (d < (circle.r + c.r)){
-      isValid = false;
-      break;
-    }
-  }
-  return isValid;
-}
-
-function drawCircles(){
-  noStroke();
-  fill(255, 0, 100, 100);
-  for(let c of circles){
-    ellipse(c.x, c.y, 2*c.r, 2*c.r);
-  }
+function mousePressed(){
+  let v = createVector(mouseX, mouseY);
+  vertices.push(v);
 }
 
 function draw(){
+  for (let v of vertices){
+    ellipse(v.x, v.y, 10, 10);
+  }
 
 }
