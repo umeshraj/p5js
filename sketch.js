0ac05949f1dfec402e276a779061e46b86bf305e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-12 12:25:01 -0400

    Namespace mode

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
-let x;
-let y;
-function setup(){
-  createCanvas(640, 480);
-  background(200);
+var sketch = function(p){
+  p.x = 100;
+  p.y = 100;
+  p.setup = function(){
+    p.createCanvas(640, 480);
+    p.background(200);
+    p.x = p.width/2;
+    p.y = p.height/2;
+  }
 
-  x = width/2;
-  y = height/2;
+  p.draw = function(){
+    p.fill(255, 0, 100, 10);
+    p.noStroke();
+    p.ellipse(p.x, p.y, 30, 30);
+    p.x += p.random(-10, 10);
+    p.y += p.random(-10, 10);
+  }
 }
 
-function draw(){
-  fill(255, 0, 100, 10);
-  noStroke();
-  ellipse(x, y, 30, 30);
-  x = x + random(-10, 10);
-  y = y + random(-10, 10);
-}
+var myp5 = new p5(sketch);
