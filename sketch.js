0de981146b53b5ebedc0d0c580040d19a8a28cea
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-13 09:19:14 -0400

    Using counter inside closure to track loading complete

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-let song1;
-let song2;
+let songs = [];
 let angle=0;
 let loading = true;
+let songCounter = 0;
 
 function preload(){
 }
@@ -9,16 +9,20 @@ function preload(){
 function setup(){
   createCanvas(300, 300);
   console.log(floor(millis()) + 'ms');
-  rainbowSong(song1, 'audio/song1.mp3');
-  rainbowSong(song2, 'audio/song2.mp3');
+  rainbowSong(0, 'audio/song1.mp3');
+  rainbowSong(1, 'audio/song2.mp3');
 }
 
 
-function rainbowSong(song, filename){
+function rainbowSong(index, filename){
   loadSound(filename, soundLoaded);
   function soundLoaded(sound){
-    song=sound;
+    songs[index] = sound;
     console.log('Loaded ' + filename);
+    songCounter++
+    if (songCounter==2){
+      loading = false;
+    }
     // song.play();
   }
 }
