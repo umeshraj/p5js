83161bc1165b545de0ba185838b08932829e1231
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-13 08:51:14 -0400

    rotating line animation

--- sketch.js
+++ sketch.js
@@ -1,47 +1,22 @@
-var sketch1 = function(p){
-  p.x = 100;
-  p.y = 100;
-  p.setup = function(){
-    p.createCanvas(200, 200);
-    p.background(200);
-    p.x = p.width/2;
-    p.y = p.height/2;
-  }
+let song1;
+let angle=0;
 
-  p.draw = function(){
-    p.fill(255, 0, 100, 10);
-    p.noStroke();
-    p.ellipse(p.x, p.y, 30, 30);
-    p.x += p.random(-10, 10);
-    p.y += p.random(-10, 10);
-  }
+function preload(){
+  song1 = loadSound('audio/yummy.mp3');
 }
 
-var sketch2 = function(p){
-  p.x = 100;
-  p.y = 100;
-  p.setup = function(){
-    p.createCanvas(200, 200);
-    p.background(200);
-    p.x = p.width/2;
-    p.y = p.height/2;
-  }
-
-  p.draw = function(){
-    p.fill(255, 255, 100, 10);
-    p.noStroke();
-    p.ellipse(p.x, p.y, 30, 30);
-    p.x += p.random(-10, 10);
-    p.y += p.random(-10, 10);
-  }
+function setup(){
+  createCanvas(300, 300);
+  console.log(floor(millis()) + 'ms');
 }
 
-var myp5_1 = new p5(sketch1);
-var myp5_2 = new p5(sketch2);
+function draw(){
+  background(51);
 
-function resetBackground(){
-  myp5_1.x = myp5_1.width/2;
-  myp5_1.y = myp5_1.height/2;
-  myp5_1.background(200);
+  translate(width/2, height/2);
+  rotate(angle);
+  strokeWeight(4);
+  stroke(255);
+  line(0, 0, 100, 0);
+  angle += 0.1;
 }
-setInterval(resetBackground, 3000);
