362f56ba1a43cef38a24057ae4030a1a66d00428
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-14 08:25:59 -0400

    Adding a particle object to the screen

--- 9p17_Arguments/sketch.js
+++ 9p17_Arguments/sketch.js
@@ -1,11 +1,21 @@
+let p;
+
 function setup(){
   createCanvas(400, 300);
-  let c = sum(10, 5);
-  console.log(c);
+  p = new Particle();
 }
 
 function draw(){
   background(51);
+  p.show();
+}
+
+function Particle(){
+  this.pos = createVector(width/2, height/2);
+  this.show = function(){
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 16, 16);
+  }
 }
 
 function sum(){
