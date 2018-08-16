8a8275c239e019385acda9aace0e8f6cbd36479f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-16 08:03:44 -0400

    Example of  curveVertex

--- 9p21_CustomShapes/sketch.js
+++ 9p21_CustomShapes/sketch.js
@@ -6,14 +6,29 @@ function setup(){
 function draw(){
   background(51);
 
+  // draw points
+  strokeWeight(12);
+  stroke('red');
+  point(100, 200);
+  point(150, 50);
+  point(250, 60);
+  point(300, 200);
+
+
   noFill();
   stroke(255);
-  let spacing = map(mouseX, 0, width, 3, 100);
+  strokeWeight(2);
+  // let spacing = map(mouseX, 0, width, 3, 100);
+
   beginShape();
-    for (let theta=0; theta<360; theta+=spacing){
-      let x = width/2 + 100 * sin(theta);
-      let y = height/2 + 100 * cos(theta);
-      vertex(x, y);
-    }
-  endShape(CLOSE);
+    // for (let theta=0; theta<360; theta+=spacing){
+    //   let x = width/2 + 100 * sin(theta);
+    //   let y = height/2 + 100 * cos(theta);
+    //   vertex(x, y);
+    // }
+    curveVertex(100, 200);
+    curveVertex(150, 50);
+    curveVertex(250, 60);
+    curveVertex(300, 200);
+  endShape();
 }
