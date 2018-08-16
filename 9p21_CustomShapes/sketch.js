aabd95ad1aa8e2ec5d61115207733154c63e8a16
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-16 07:48:08 -0400

    Simple shape

---
+++ 9p21_CustomShapes/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(400, 300);
+  background(51);
+
+  noFill();
+  stroke(255);
+  beginShape();
+  vertex(10, 20);
+  vertex(12, 89);
+  vertex(88, 132);
+  vertex(45, 23);
+  endShape(CLOSE);
+}
+
+// function draw(){
+//   background(51);
+// }
