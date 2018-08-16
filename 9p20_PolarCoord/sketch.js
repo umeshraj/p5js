e209833d90ad863168cf2487b68e9d4c23fadc45
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-16 07:21:24 -0400

    Draw a point based on r, theta

--- 9p20_PolarCoord/sketch.js
+++ 9p20_PolarCoord/sketch.js
@@ -1,12 +1,22 @@
 function setup(){
   createCanvas(400, 300);
+  angleMode(DEGREES);
 }
 
 function draw(){
   background(51);
   let x = 100;
-  let y = 300;
+  let y = 200;
   stroke(255);
   strokeWeight(10);
   point(x, y);
+
+  let angle = -45;
+  let r = 100; // 100 pixels away
+  dx = r * cos(angle);
+  dy = r * sin(angle);
+  stroke('red');
+  point(x+dx, y+dy);
+  strokeWeight(2);
+  line(x, y, x+dx, y+dy);
 }
