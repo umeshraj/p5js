b8b13c94a9e3439e4022b65a71ae097140c975e6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-16 07:55:51 -0400

    Circle to triangle based on mouseX

--- 9p21_CustomShapes/sketch.js
+++ 9p21_CustomShapes/sketch.js
@@ -1,11 +1,14 @@
 function setup(){
   angleMode(DEGREES);
   createCanvas(400, 300);
+}
+
+function draw(){
   background(51);
 
   noFill();
   stroke(255);
-  let spacing = 50;
+  let spacing = map(mouseX, 0, width, 3, 100);
   beginShape();
     for (let theta=0; theta<360; theta+=spacing){
       let x = width/2 + 100 * sin(theta);
@@ -14,7 +17,3 @@ function setup(){
     }
   endShape(CLOSE);
 }
-
-// function draw(){
-//   background(51);
-// }
