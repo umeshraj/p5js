08de3831658534fde97322b81e0570e5236e5676
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-17 19:52:08 -0400

    Adding a small create graphics option

--- 9p23_createGraphics/sketch.js
+++ 9p23_createGraphics/sketch.js
@@ -1,17 +1,21 @@
 let graphics;
+let x = 50;
+let y = 50;
 
 function setup(){
   createCanvas(400, 300);
-  graphics = createGraphics(400, 300);
+  graphics = createGraphics(100, 100);
   graphics.background(51);
 }
 
 function draw(){
-  if (mouseIsPressed){
-    graphics.stroke(255);
-    graphics.fill(255);
-    graphics.ellipse(mouseX, mouseY, 32, 32);
-  }
+  background(0);
+  graphics.stroke(255);
+  graphics.fill(255);
+  graphics.ellipse(x, y, 5, 5);
+  x += random(-5, 5);
+  y += random(-5, 5);
+
   image(graphics, 0, 0);
   fill(255, 0, 255);
   ellipse(mouseX, mouseY, 8, 8);
