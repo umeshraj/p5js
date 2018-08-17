8fefc2409f1de2d22155e2e1d18347e7ab884426
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-17 19:48:39 -0400

    Using create graphics to make pen tip

--- 9p23_createGraphics/sketch.js
+++ 9p23_createGraphics/sketch.js
@@ -3,15 +3,16 @@ let graphics;
 function setup(){
   createCanvas(400, 300);
   graphics = createGraphics(400, 300);
-  background(51);
+  graphics.background(51);
 }
 
 function draw(){
   if (mouseIsPressed){
-    stroke(255);
-    fill(255);
-    ellipse(mouseX, mouseY, 32, 32);
+    graphics.stroke(255);
+    graphics.fill(255);
+    graphics.ellipse(mouseX, mouseY, 32, 32);
   }
+  image(graphics, 0, 0);
   fill(255, 0, 255);
   ellipse(mouseX, mouseY, 8, 8);
 }
