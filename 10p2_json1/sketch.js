df268b7556b1d148bfddb2494200d777ccf60177
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-18 08:09:49 -0400

    Loading json from a text file with preload

--- 10p2_json1/sketch.js
+++ 10p2_json1/sketch.js
@@ -1,15 +1,15 @@
 let flower;
 
+function preload(){
+  flower = loadJSON("flower.json");
+}
+
 function setup(){
   createCanvas(400, 300);
-  flower = {
-    name: "sunflower",
-    col: color(200, 220, 0)
-  };
 }
 
 function draw(){
   background(51);
-  fill(flower.col);
+  fill(flower.r, flower.g, flower.b);
   text(flower.name, 10, 50);
 }
