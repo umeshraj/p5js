679ddbf2379885b735d3a9fbbdc7f54501fa8227
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-19 10:12:24 -0400

    Adding ellipse based on data from api

--- 10p5_apis/sketch.js
+++ 10p5_apis/sketch.js
@@ -1,13 +1,22 @@
+let weather;
+
 function setup(){
   createCanvas(400, 300);
-  let url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=***REMOVED***&units=metric"
+  let url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=***REMOVED***&units=metric  "
   loadJSON(url, gotData);
 }
 
 function gotData(data){
   console.log(data);
+  weather = data;
 }
 
 function draw(){
   background(51);
+  if(weather){
+    fill('pink');
+    ellipse(50, 100, weather.main.temp, weather.main.temp);
+    fill('blue');
+    ellipse(150, 100, weather.main.humidity, weather.main.humidity);
+  }
 }
