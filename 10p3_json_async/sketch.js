47c34cdb4bb076cb174f772a7bcc026a3a69748b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-19 09:30:57 -0400

    Using global variable to plot async data

--- 10p3_json_async/sketch.js
+++ 10p3_json_async/sketch.js
@@ -1,3 +1,5 @@
+let spaceData;
+
 function setup(){
   createCanvas(400, 300);
 
@@ -6,15 +8,19 @@ function setup(){
 }
 
 function gotData(data){
-  background(51);
   console.log("finished loading data...")
   console.log(data.number);
-  for (let idx=0; idx<data.number; idx++){
-    fill('pink');
-    ellipse(random(width), random(height), 20, 20);
-  }
+  spaceData = data;
 }
 
 function draw(){
+  background(51);
+  randomSeed(4);
 
+  if (spaceData){
+    for (let idx=0; idx<spaceData.number; idx++){
+      fill('pink');
+      ellipse(random(width), random(height), 20, 20);
+    }
+  }
 }
