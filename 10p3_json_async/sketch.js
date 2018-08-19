7f9c828ccf4800acd3c10799bb3d56c6b3a2ea5b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-19 09:22:42 -0400

    Async loading of data from url

--- 10p3_json_async/sketch.js
+++ 10p3_json_async/sketch.js
@@ -1,11 +1,18 @@
 function setup(){
+  createCanvas(400, 300);
+
   console.log("loading data...")
-  loadJSON("birds_antarctica.json", gotData)
+  loadJSON("http://api.open-notify.org/astros.json", gotData)
 }
 
 function gotData(data){
+  background(51);
   console.log("finished loading data...")
-  console.log(data);
+  console.log(data.number);
+  for (let idx=0; idx<data.number; idx++){
+    fill('pink');
+    ellipse(random(width), random(height), 20, 20);
+  }
 }
 
 function draw(){
