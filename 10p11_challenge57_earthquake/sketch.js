c168e2decd10dd4de6f8278ccc7da1fea0b2e7b7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-23 08:56:57 -0400

    Fixing map scale to plot cities correctly

--- 10p11_challenge57_earthquake/sketch.js
+++ 10p11_challenge57_earthquake/sketch.js
@@ -3,9 +3,10 @@ let zoom = 1;
 // centra lat/lon
 let clat = 0;
 let clon = 0;
-// 31.2304° N, 121.4737° E
-let lat = 31.2304;
-let lon = 121.4737;
+// Shanghai 31.2304° N, 121.4737° E
+// Vancouver: 49.2827° N, 123.1207° W
+let lat = 49.2827;
+let lon = -123.1207;
 
 
 function preload(){
@@ -24,20 +25,20 @@ function setup(){
   let x = mercX(lon) - cx;
   let y = mercY(lat) - cy;
   fill(255, 0, 255, 200);
-  ellipse(x, y, 20, 20);
+  ellipse(x, y, 10, 10);
 }
 
 // mapping long to x,y
 function mercX(lon){
   lon = radians(lon);
-  let a = 128/(PI) * pow(2, zoom);
+  let a = 256/(PI) * pow(2, zoom);
   let b = (lon + PI);
   return a * b;
 }
 
 function mercY(lat){
   lat = radians(lat);
-  let a = 128/(PI) * pow(2, zoom);
+  let a = 256/(PI) * pow(2, zoom);
   let b = tan(PI/4 + lat/2);
   let c = PI - log(b);
   return a * c;
