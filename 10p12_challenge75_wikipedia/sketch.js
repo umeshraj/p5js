66f0751a08ba326e90145e4fbd69e3a05028fdbd
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-28 20:40:18 -0400

    Randomly pick a word from the content

--- 10p12_challenge75_wikipedia/sketch.js
+++ 10p12_challenge75_wikipedia/sketch.js
@@ -1,5 +1,6 @@
 let userInput;
 let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
 
 function setup(){
   noCanvas();
@@ -11,13 +12,28 @@ function setup(){
 function goWiki(){
   let term = userInput.value();
   let url = searchUrl + term;
-  loadJSON(url, gotData, "jsonp")
+  loadJSON(url, gotSearch, "jsonp")
 }
 
-function gotData(data){
+function gotSearch(data){
   //randomly pick an element
   let title = random(data[1]);
+  createP(title);
   // replace all spaces with _
   title = title.replace(/\s+/g, '_')
-  console.log(title);
+  console.log("Quering: " + title);
+  let url = contentUrl + title;
+  console.log(url);
+  loadJSON(url, gotContent, 'jsonp');
+}
+
+function gotContent(data){
+  let page = data.query.pages;
+  let pageId = Object.keys(data.query.pages)[0];
+  let content = page[pageId]["revisions"][0]["*"]
+
+  let wordRegex = /\b\w{4,}\b/g;
+  let words = content.match(wordRegex);
+  let word = random(words);
+  console.log(word);
 }
