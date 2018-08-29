60cf8bb29116be0bc04260dacb27a6fe73900669
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-28 20:19:38 -0400

    Changing to p5.js 0.7.1 to avoid jsonp error

--- 10p12_challenge75_wikipedia/sketch.js
+++ 10p12_challenge75_wikipedia/sketch.js
@@ -1,11 +1,19 @@
 let userInput;
+let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+
 function setup(){
   noCanvas();
   userInput = select('#userinput');
   userInput.changed(goWiki);
+  goWiki();  // force printing of what is in the input box
 }
 
 function goWiki(){
   let term = userInput.value();
-  console.log(term);
+  let url = searchUrl + term;
+  loadJSON(url, gotData, "jsonp")
+}
+
+function gotData(){
+  // console.log(data);
 }
