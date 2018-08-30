aedf5c288f92d5e9cd365f52fb376eb160b86c4f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 07:43:14 -0400

    Mapping each pixel in video to 16*18 canvas pixels

--- 11p4_brightness_mirror/sketch.js
+++ 11p4_brightness_mirror/sketch.js
@@ -1,30 +1,27 @@
 let video;
+let vScale = 16;
 
 function setup(){
-  let my_w = 320;
-  let my_h = 240;
-  createCanvas(my_w, my_h);
+  createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(my_w, my_h);
+  video.size(width/vScale, height/vScale);
 }
 
 function draw(){
   background(51);
   video.loadPixels();
-  loadPixels();
-  for (let y=0; y<height; y++){
-    for (let x=0; x<width; x++){
-        let idx = (x +y*width) * 4;
+  // loadPixels();
+  for (let y=0; y<video.height; y++){
+    for (let x=0; x<video.width; x++){
+        let idx = (x +y*video.width) * 4;
         let r = video.pixels[idx+0];
         let g = video.pixels[idx+1];
         let b = video.pixels[idx+2];
 
         let bright = (r+g+b)/3;
-        pixels[idx+0] = bright;
-        pixels[idx+1] = bright;
-        pixels[idx+2] = bright;
-        pixels[idx+3] = 255;
+        fill(bright);
+        rect(x*vScale, y*vScale, vScale, vScale);
     }
   }
-  updatePixels();
+  // updatePixels();
 }
