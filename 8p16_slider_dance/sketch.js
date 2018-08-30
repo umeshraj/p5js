17da722cbeab0a8a018f2903b4a5fdf336c39751
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 08:33:09 -0400

    Let slider change with sine

--- 8p16_slider_dance/sketch.js
+++ 8p16_slider_dance/sketch.js
@@ -2,10 +2,11 @@ let slider;
 let angle = 0;
 function setup(){
   noCanvas();
-  slider = createSlider(0, 100, 50);
+  slider = createSlider(0, 255, 50);
 }
 
 function draw(){
-  let x = sin(0);
+  let x = map(sin(angle), -1, 1, 0, 255) ;
   slider.value(x);
+  angle += 0.1;
 }
