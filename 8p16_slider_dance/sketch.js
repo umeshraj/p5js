1485793396a0f5d616137e9577a179fba2f287e6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 08:38:27 -0400

    Add slight offset to slider to dance

--- 8p16_slider_dance/sketch.js
+++ 8p16_slider_dance/sketch.js
@@ -3,15 +3,17 @@ let sliders = [];
 let angle = 0;
 function setup(){
   noCanvas();
-  for (let i=0;i<10;i++){
+  for (let i=0;i<100;i++){
     sliders[i] = createSlider(0, 255, 50);
   }
 }
 
 function draw(){
-  let x = map(sin(angle), -1, 1, 0, 255);
+  let offset = 0;
   for (let i=0; i<sliders.length;i++){
-    sliders[i].value(x);  
+    let x = map(sin(angle+offset), -1, 1, 0, 255);
+    sliders[i].value(x);
+    offset += 0.025;
   }
 
   angle += 0.1;
