1d14a75866f4f69ccc6d6baea81d0c04fd3f9e94
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 07:37:42 -0400

    rgb to grayscale

--- 11p4_brightness_mirror/sketch.js
+++ 11p4_brightness_mirror/sketch.js
@@ -1,16 +1,28 @@
+let video;
+
 function setup(){
-  createCanvas(320, 240);
+  let my_w = 320;
+  let my_h = 240;
+  createCanvas(my_w, my_h);
+  video = createCapture(VIDEO);
+  video.size(my_w, my_h);
 }
 
 function draw(){
   background(51);
+  video.loadPixels();
   loadPixels();
   for (let y=0; y<height; y++){
     for (let x=0; x<width; x++){
         let idx = (x +y*width) * 4;
-        pixels[idx+0] = x;
-        pixels[idx+1] = random(0, 255);
-        pixels[idx+2] = y;
+        let r = video.pixels[idx+0];
+        let g = video.pixels[idx+1];
+        let b = video.pixels[idx+2];
+
+        let bright = (r+g+b)/3;
+        pixels[idx+0] = bright;
+        pixels[idx+1] = bright;
+        pixels[idx+2] = bright;
         pixels[idx+3] = 255;
     }
   }
