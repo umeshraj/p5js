210911612995ed759cc8065eba30be5f1ecb7489
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 07:13:13 -0400

    Setting every pixel value in the background to a color

--- 11p3_pixel_array/sketch.js
+++ 11p3_pixel_array/sketch.js
@@ -5,9 +5,14 @@ function setup(){
 function draw(){
   background(51);
   loadPixels();
-  pixels[0] = 255;
-  pixels[1] = 255;
-  pixels[2] = 255;
-  pixels[3] = 255;
+  for (let y=0; y<height; y++){
+    for (let x=0; x<width; x++){
+        let idx = (x +y*width) * 4;
+        pixels[idx+0] = x;
+        pixels[idx+1] = 0;
+        pixels[idx+2] = y;
+        pixels[idx+3] = 255;
+    }
+  }
   updatePixels();
 }
