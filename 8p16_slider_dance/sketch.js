35e456d0c0ceb7d0455db04ad53d688733e35b7b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 08:42:26 -0400

    Make all sliders move based on the first one

--- 8p16_slider_dance/sketch.js
+++ 8p16_slider_dance/sketch.js
@@ -6,14 +6,12 @@ function setup(){
   for (let i=0;i<100;i++){
     sliders[i] = createSlider(0, 255, 50);
   }
+  sliders[0].input(adjustSliders);
 }
 
-function draw(){
-  let offset = 0;
-  for (let i=0; i<sliders.length;i++){
-    let x = map(sin(angle+offset), -1, 1, 0, 255);
-    sliders[i].value(x);
-    offset += 0.025;
+function adjustSliders(){
+  for (let i=1; i<sliders.length;i++){
+    sliders[i].value(sliders[0].value());
   }
 
   angle += 0.1;
