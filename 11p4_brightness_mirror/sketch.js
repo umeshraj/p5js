595018f0405508500031bc628949722bb8f6eb2b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 07:16:43 -0400

    First commit of 11p3 code

---
+++ 11p4_brightness_mirror/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+  createCanvas(320, 240);
+}
+
+function draw(){
+  background(51);
+  loadPixels();
+  for (let y=0; y<height; y++){
+    for (let x=0; x<width; x++){
+        let idx = (x +y*width) * 4;
+        pixels[idx+0] = x;
+        pixels[idx+1] = random(0, 255);
+        pixels[idx+2] = y;
+        pixels[idx+3] = 255;
+    }
+  }
+  updatePixels();
+}
