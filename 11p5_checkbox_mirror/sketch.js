18e174fd59c9cfaaa2b26b8b0923ec9b6243ea96
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-08-30 08:02:47 -0400

    Put checkboxes into a div

--- 11p5_checkbox_mirror/sketch.js
+++ 11p5_checkbox_mirror/sketch.js
@@ -1,11 +1,21 @@
 let video;
 let vScale = 16;
 let threshold = 127;
+let rows = 40;  // number of rows of check boxes
+let cols = 30;  // number of cols of check boxes
 
 function setup(){
   createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(width/vScale, height/vScale);
+  video.size(cols, rows);
+
+  // create check boxes
+  for(let x=0; x<cols; x++){
+    for(let y=0; y<rows; y++){
+      let box = createCheckbox();
+      box.parent('mirror');
+    }
+  }
 }
 
 function draw(){
@@ -25,6 +35,7 @@ function draw(){
         }else{
           fill(0);
         }
+        // noStroke();
         rectMode(CENTER);
         rect(x*vScale, y*vScale, vScale, vScale);
     }
