5b38310214f04377ca9112d3cf826a2dd87241a9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-24 08:09:08 -0400

    Adding slider for blur

--- 11p8_video_effects_seriously/sketch.js
+++ 11p8_video_effects_seriously/sketch.js
@@ -1,4 +1,5 @@
 let video;
+let slider;
 
 function setup(){
   canvas = createCanvas(400, 300, WEBGL);
@@ -6,13 +7,16 @@ function setup(){
   video = createCapture(VIDEO);
   video.size(400, 300);
   video.id('p5video');
+  video.hide();
   canvas.id('p5canvas');
+  slider = createSlider(0, 1, 0.5, 0.01);
+  slider.id('blur_slider')
 
   let seriously = new Seriously();
   let src = seriously.source('#p5video');
   let target = seriously.target('#p5canvas');
   let blur = seriously.effect('blur');
-
+  blur.amount = '#blur_slider';
   blur.source = src;
   target.source = blur;
 
