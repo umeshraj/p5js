2d9481ffb46e7207ec079e92c8459d6804a7d0cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-26 09:00:13 -0400

    Client is sending data to server via emit

--- 12p1_node_sockets/public/sketch.js
+++ 12p1_node_sockets/public/sketch.js
@@ -6,8 +6,17 @@ function setup(){
   socket = io.connect('http://localhost:3000');
 }
 
-function draw(){
+function mouseDragged(){
+  console.log('Sending: ' + mouseX + ',' + mouseY);
+  var data = {
+    x: mouseX,
+    y: mouseY
+  };
+  socket.emit('mouse', data);
   noStroke();
   fill(255);
   ellipse(mouseX, mouseY, 60, 60);
+}
+
+function draw(){
 }
