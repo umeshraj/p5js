9f3bf749ec6e1803fa8f67ad05b0852d7c347576
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-26 08:51:33 -0400

    Server side add socket io to show console message on connect

--- 12p1_node_sockets/server.js
+++ 12p1_node_sockets/server.js
@@ -6,3 +6,12 @@ var server = app.listen(3000);
 app.use(express.static('public'));
 
 console.log("My socket server is running");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log('new connection:' + socket.id);
+}
