13d63c10e8b5a7e899fe59e1ebc3844f27d3c6ef
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-29 18:57:55 -0400

    Broadcast client changes to server and to other clients

--- 12p1_node_sockets/public/sketch.js
+++ 12p1_node_sockets/public/sketch.js
@@ -4,6 +4,13 @@ function setup(){
   background(51);
 
   socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data){
+  noStroke();
+  fill(255,0, 100);
+  ellipse(data.x, data.y, 36, 36);
 }
 
 function mouseDragged(){
