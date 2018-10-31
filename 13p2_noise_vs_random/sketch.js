0147b50df84f6f2f7584894226d44e8dd7eef187
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-31 08:41:12 -0400

    2d perlin noise along x axis

--- 13p2_noise_vs_random/sketch.js
+++ 13p2_noise_vs_random/sketch.js
@@ -5,15 +5,18 @@ function setup(){
 }
 
 function draw(){
+  var xoff = 0;
   loadPixels();
-  for (var x=0; x<width; x++){
-    for (var y=0; y<height; y++){
+  for (var y=0; y<height; y++){
+    for (var x=0; x<width; x++){
       var index = (x + y * width) * 4;
-      var r = random(255);
+      var r = noise(xoff) * 255;
       pixels[index+0] = r;
       pixels[index+1] = r;
       pixels[index+2] = r;
       pixels[index+3] = 255;
+
+      xoff += 0.01;
     }
   }
   updatePixels();
