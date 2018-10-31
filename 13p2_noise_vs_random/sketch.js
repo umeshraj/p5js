609e829e3901e0bbcef021d1d7621aa498098c3a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-31 08:38:45 -0400

    Random noise image

--- 13p2_noise_vs_random/sketch.js
+++ 13p2_noise_vs_random/sketch.js
@@ -9,9 +9,10 @@ function draw(){
   for (var x=0; x<width; x++){
     for (var y=0; y<height; y++){
       var index = (x + y * width) * 4;
-      pixels[index+0] = 255;
-      pixels[index+1] = 0;
-      pixels[index+2] = 0;
+      var r = random(255);
+      pixels[index+0] = r;
+      pixels[index+1] = r;
+      pixels[index+2] = r;
       pixels[index+3] = 255;
     }
   }
