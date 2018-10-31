2b7b94973629adae86f79e072b4190ea8fbab234
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-31 08:45:53 -0400

    2d perlin noise

--- 13p2_noise_vs_random/sketch.js
+++ 13p2_noise_vs_random/sketch.js
@@ -5,19 +5,20 @@ function setup(){
 }
 
 function draw(){
-  var xoff = 0;
   loadPixels();
+  var yoff = 0;
   for (var y=0; y<height; y++){
+    var xoff = 0;
     for (var x=0; x<width; x++){
       var index = (x + y * width) * 4;
-      var r = noise(xoff) * 255;
+      var r = noise(xoff, yoff) * 255;
       pixels[index+0] = r;
       pixels[index+1] = r;
       pixels[index+2] = r;
       pixels[index+3] = 255;
-
-      xoff += 0.01;
+      xoff += incr;
     }
+    yoff += incr;
   }
   updatePixels();
 }
