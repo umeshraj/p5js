6aacedd20c323593eaae46e91558964184be0ecf
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-31 08:37:10 -0400

    Using loadpixels() to make an 'image'

--- 13p2_noise_vs_random/sketch.js
+++ 13p2_noise_vs_random/sketch.js
@@ -1,30 +1,19 @@
-// let xoff1 = 0;
-// let xoff2 = 10000;
 let incr = 0.01;
-let start = 0;
+
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
 
 function draw(){
-  background(51);
-  stroke(255);
-  noFill();
-
-  let xoff = start;
-  beginShape();
+  loadPixels();
   for (var x=0; x<width; x++){
-    var n = map(noise(xoff), 0, 1, -50, 50);
-    var s = map(sin(xoff), -1, 1, 0, height);
-    var y = n + s;
-    stroke(255);
-    // y = random(height);
-    // y = noise(xoff) * height;
-    vertex(x, y);
-    xoff += incr;
+    for (var y=0; y<height; y++){
+      var index = (x + y * width) * 4;
+      pixels[index+0] = 255;
+      pixels[index+1] = 0;
+      pixels[index+2] = 0;
+      pixels[index+3] = 255;
+    }
   }
-  endShape();
-  start += incr;
-
-  // noLoop();
+  updatePixels();
 }
