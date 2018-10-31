946558c51a4fc6a050d89b4b4e84a4cb65a30f9f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-10-31 08:16:13 -0400

    x vs t plot of random noise

--- 13p2_noise_vs_random/sketch.js
+++ 13p2_noise_vs_random/sketch.js
@@ -1,5 +1,5 @@
-let xoff1 = 0;
-let xoff2 = 10000;
+// let xoff1 = 0;
+// let xoff2 = 10000;
 
 function setup(){
   createCanvas(400, 400);
@@ -7,10 +7,20 @@ function setup(){
 
 function draw(){
   background(51);
-  var x = map(noise(xoff1), 0, 1, 0, width);
-  var y = map(noise(xoff2), 0, 1, 0, height);
+  stroke(255);
+  noFill();
+  // var x = map(noise(xoff1), 0, 1, 0, width);
+  // var y = map(noise(xoff2), 0, 1, 0, height);
 
-  xoff1 += 0.01;
-  xoff2 += 0.01;
-  ellipse(x, y, 24, 24);
+  // xoff1 += 0.01;
+  // xoff2 += 0.01;
+  // ellipse(x, y, 24, 24);
+
+  beginShape();
+  for (var x=0; x<width; x++){
+    stroke(255);
+    vertex(x, random(height));
+  }
+  endShape();
+  noLoop();
 }
