eb6cf65b81f09041639936f39ba0e2fc312c990f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-01 08:19:43 -0400

    Adding button to start simulation

--- 14p2_hamilton_code/sketch.js
+++ 14p2_hamilton_code/sketch.js
@@ -1,15 +1,25 @@
 var prob = 50;
+var button;
+var started = false;
 
 function setup(){
+    noCanvas();
+    button = select("#start");
+    button.mousePressed(startLottery);
+}
 
+function startLottery(){
+    started = true;
 }
 
 function draw(){
-    var r = floor(random(prob));
+    if (started){
+        var r = floor(random(prob));
 
-    if (r == 1){
-        console.log("Won lottery!!!!!");
-    }else{
-        console.log("Lost lottery");
+        if (r == 1){
+            console.log("Won lottery!!!!!");
+        }else{
+            console.log("Lost lottery");
+        }    
     }
 }
