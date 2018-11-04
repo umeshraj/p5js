757670782a3973269dd59a9e877b40dd41a94183
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-04 10:54:52 -0500

    setInterval to tweet every 10s

--- 15p5_scheduling_tweets/bot_post.js
+++ 15p5_scheduling_tweets/bot_post.js
@@ -4,7 +4,9 @@ var Twit = require('twit');
 var config = require('./config')
 
 var T = new Twit(config);
+
 tweetIt();
+setInterval(tweetIt, 1000*10);
 
 function tweetIt(){
     var r = Math.floor(Math.random()*100);
