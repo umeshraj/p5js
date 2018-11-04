4d4fc981e57b4dc37927500936c04333114836d5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-04 10:25:34 -0500

    Printing specific texts from returned tweet

--- 15p4_twitter_api_basics/bot.js
+++ 15p4_twitter_api_basics/bot.js
@@ -7,11 +7,15 @@ console.log(config);
 var T = new Twit(config);
 var params = { 
     q: 'rainbow', 
-    count: 2
+    count: 10,
+    lang: 'en'
 }
 
 T.get('search/tweets', params, gotData); 
 
 function gotData(err, data, response) {
-    console.log(data)
+    var tweets = data.statuses;
+    for(var i=0; i<tweets.length; i++){
+        console.log(tweets[i].text);
+    }
 };
