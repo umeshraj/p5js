d79edada801c4015b6cc94b6a169c542071482bd
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-05 08:07:13 -0500

    Posting image from node

--- 15p6_tweet_images/bot_post.js
+++ 15p6_tweet_images/bot_post.js
@@ -4,17 +4,29 @@ var Twit = require('twit');
 var config = require('./config')
 var T = new Twit(config);
 
+var fs = require('fs');
+
 tweetIt();
-setInterval(tweetIt, 1000*10);
+// setInterval(tweetIt, 1000*10);
 
 function tweetIt(){
-    var r = Math.floor(Math.random()*100);
-
-    var tweet = {
-        status: r + 'hello world from node.js'
+    var filename = 'flower.jpg';
+    var params = {
+        encoding: 'base64'
     }
-    T.post('statuses/update', tweet, tweeted);
-    
+    var b64 = fs.readFileSync(filename, params);
+    T.post('media/upload', {media_data: b64}, uploaded);
+
+    function uploaded(err, data, response){
+        // This is where I will tweet
+        var id = data.media_id_string;
+        var tweet = {
+            status: 'image world from node.js',
+            media_ids: [id]
+        }
+        T.post('statuses/update', tweet, tweeted);
+    }
+
     function tweeted(err, data, response) {
         if (err){
             console.log("Something went wrong!");
