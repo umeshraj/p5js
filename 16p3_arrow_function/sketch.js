f82e40a55cb1d835810fb382d3142fd451c5b507
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-10 08:22:34 -0500

    Convert to an inline anonymous function without name

--- 16p3_arrow_function/sketch.js
+++ 16p3_arrow_function/sketch.js
@@ -3,12 +3,11 @@ function setup(){
   background(0);
   let button = createButton('press');
 
-  button.mousePressed(changeBackground);
-  
+  button.mousePressed(function (){
+    background(random(255));
+  });
+
 }
 
-function changeBackground(){
-  background(random(255));
-}
 
 
