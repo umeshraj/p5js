a5f0a9416ece875b5db69a5977b811422eabe565
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-10 09:34:45 -0500

    Writing countIt() in a function for clarity

--- 16p3_arrow_function/sketch.js
+++ 16p3_arrow_function/sketch.js
@@ -1,22 +1,29 @@
-let counter1;
-
 function setup(){
   noCanvas();
-  counter1 = new Counter(100, 500);
-}
-
-function draw(){
-  counter1.countIt();
+  const counter1 = new Counter(100, 500);
+  counter1.start();
+  const counter2 = new Counter(500, 200);
+  counter2.start();
+  const counter3 = new Counter(1000, 1000);
+  counter3.start();
 }
 
 class Counter{
   constructor(start, wait){
     this.count = 0;
+    this.wait = wait;
     this.p = createP('');
- 
-    setInterval(()=>{
-      this.count++;
-      this.p.html(this.count);
-    } , wait);
   }
+ 
+  start(){
+    setInterval(()=>{ 
+      this.countIt();     
+    }, this.wait);
+  }
+
+  countIt(){
+    this.count++;
+    this.p.html(this.count);
+  }
+  
 }
