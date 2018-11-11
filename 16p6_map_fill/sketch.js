a5a28e925995588fe32f868f641d75b382c06d7d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-11 11:24:30 -0500

    First example of map() for doubling array

--- 16p6_map_fill/sketch.js
+++ 16p6_map_fill/sketch.js
@@ -1,9 +1,12 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
-function setup(){
-  createCanvas(400, 300);
+let vals = [4, 8, 1, 2, 9];
+console.log(vals);
+
+
+function doubler(x){
+  return x*2;
 }
 
-function draw(){
-  background(51);
-}
+let doubled = vals.map(doubler)
+console.log(doubled);
