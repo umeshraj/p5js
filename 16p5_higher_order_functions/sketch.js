10f77eab14819dc030d9203afe30aece02e960bd
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-11 11:13:07 -0500

    Add example of using function factory

--- 16p5_higher_order_functions/sketch.js
+++ 16p5_higher_order_functions/sketch.js
@@ -11,11 +11,13 @@ function meow(){
   console.log('meow meow');
 }
 
-function setup(){
-  
-}
 
 function multiplier(factor){
   return x => x * factor;
 }
+
+let doubler = multiplier(2);
+let tripler = multiplier(3);
+console.log(doubler(5));
+console.log(tripler(7));
 
