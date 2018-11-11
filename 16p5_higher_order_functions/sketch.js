98b2aeedec069f9d19a97fee5a84d571631558bb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-11 10:51:17 -0500

    Simple callback fn passed to sing()

--- 16p5_higher_order_functions/sketch.js
+++ 16p5_higher_order_functions/sketch.js
@@ -1,9 +1,13 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
-function sing(){
+function sing(callback){
   console.log('la la la la');
+  callback();
 }
 
+function meow(){
+  console.log('meow meow');
+}
 
 function setup(){
   
