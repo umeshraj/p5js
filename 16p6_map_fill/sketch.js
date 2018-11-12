0fbac962e4e9ea7fef8fea5221bcf9f57d80bec3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-12 07:23:46 -0500

    Using arrow fn for doubler

--- 16p6_map_fill/sketch.js
+++ 16p6_map_fill/sketch.js
@@ -10,10 +10,15 @@ function doubler(x){
 
 //  Regular call to fn
 let doubled = vals.map(doubler)
+
 // Anonymous version
 let doubledAnonymous = vals.map(function (x){
   return x*2;
 });
 
+// Arrow functions
+let doubleArrow = vals.map(x => x*2);
+
 console.log("Regular: " + doubled);
 console.log("Anonymous: " + doubledAnonymous)
+console.log("Arrow: " + doubleArrow)
