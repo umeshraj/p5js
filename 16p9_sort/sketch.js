58552ac495210ed2603f4a1ca64a6a07272efda3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-13 08:02:37 -0500

    String sorting

--- 16p9_sort/sketch.js
+++ 16p9_sort/sketch.js
@@ -4,3 +4,9 @@ let vals = [5, 4, 4, 2, 1];
 console.log(vals);
 vals.sort();
 console.log(vals);
+
+// String example
+let string = ["a", "hello", "B", "goodbye"];
+console.log(string);
+string.sort();
+console.log(string);
