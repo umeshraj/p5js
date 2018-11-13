a84cb08ceb1852c90e5f5e34a97793a46b145ff9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-13 08:10:21 -0500

    Using sort with a comparison function

--- 16p9_sort/sketch.js
+++ 16p9_sort/sketch.js
@@ -10,3 +10,18 @@ let string = ["a", "hello", "B", "goodbye"];
 console.log(string);
 string.sort();
 console.log(string);
+
+// Sorting objects
+let objects = [{x: 5, y:6}, {x:2, y:10}];
+console.log(objects);
+console.log("Without comparisong fn: ")
+objects.sort();
+console.log(objects);
+
+function compareXY(a, b){
+  return a.y - b.y;
+}
+
+objects.sort(compareXY);
+console.log("After using compareXY fn in sort:")
+console.log(objects);
