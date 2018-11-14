efc4f32cf246a4120847fec50a9df7d1ede5b7c6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-14 07:12:22 -0500

    Create and show single particle

--- 16p10_particles_array/sketch.js
+++ 16p10_particles_array/sketch.js
@@ -1,9 +1,25 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 function setup(){
-  createCanvas(400, 300);
+  createCanvas(600, 400);
+  p = new Particle();
+
 }
 
 function draw(){
   background(51);
+  p.show();
+}
+
+class Particle{
+  constructor(){
+    this.x = 300;
+    this.y = 380;
+  }
+
+  show(){
+    stroke(255);
+    fill(255, 10);
+    ellipse(this.x, this.y, 16);
+  }
 }
