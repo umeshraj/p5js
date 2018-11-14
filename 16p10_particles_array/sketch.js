b3da7d8bb5a26d00b2f8b2ff90ef241ea89fc128
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-14 08:13:21 -0500

    Using filter instead of splice to remove old particles

--- 16p10_particles_array/sketch.js
+++ 16p10_particles_array/sketch.js
@@ -14,13 +14,20 @@ function draw(){
     particles.push(p);  
   }
 
-  for (let i=particles.length-1; i>=0; i--){    
-    particles[i].update();
-    particles[i].show();
-    if (particles[i].finished()){
-      particles.splice(i, 1);
-    }
+  for (let particle of particles){
+    particle.update();
+    particle.show();
   }
+
+  particles = particles.filter(p => !p.finished());
+
+  // for (let i=particles.length-1; i>=0; i--){    
+  //   particles[i].update();
+  //   particles[i].show();
+  //   if (particles[i].finished()){
+  //     particles.splice(i, 1);
+  //   }
+  // }
 }
 
 class Particle{
