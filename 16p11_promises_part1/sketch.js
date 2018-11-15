720b7b2172567b363782892081186be90c3ef601
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-15 08:00:13 -0500

    Simple promise to fetch from wordnik

--- 16p11_promises_part1/sketch.js
+++ 16p11_promises_part1/sketch.js
@@ -1,7 +1,22 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
+const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=5&maxLength=-1&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
+
+
 function setup(){
-  createCanvas(400, 300);
+  noCanvas();
+  let promise = fetch(wordnikAPI);
+  console.log(promise);
+  promise.then(gotData);
+  promise.catch(gotErr);
+}
+
+function gotData(data){
+  console.log(data);
+}
+
+function gotErr(err){
+  console.log(err);
 }
 
 function draw(){
