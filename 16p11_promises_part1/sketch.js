c5234d23d96613fa7a7482f13cb43b0fdf7a9d8e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-16 07:21:58 -0500

    Adding explicit return for promise chain to work

--- 16p11_promises_part1/sketch.js
+++ 16p11_promises_part1/sketch.js
@@ -1,6 +1,7 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=5&maxLength=-1&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
+const giphyAPI = "https://api.giphy.com/v1/gifs/search?rating=PG&api_key=dc6zaTOxFJmzC&q=";
 
 
 function setup(){
@@ -20,8 +21,19 @@ function setup(){
 
   // Getting data in JSON format
   fetch(wordnikAPI)
-    .then(response => response.json())
-    .then(json => createP(json.word))
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+        createP(json.word);
+        return fetch(giphyAPI + json.word);
+      })
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      createImg(json.data[0].images['fixed_height_small'].url);
+    })
     .catch(err => console.log(err));
 }
 
