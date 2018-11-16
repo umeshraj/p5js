422a31e54ff7b8a4e5382226634f39daf97469d5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-16 07:25:48 -0500

    Remove some unwanted returns to keep it short

--- 16p11_promises_part1/sketch.js
+++ 16p11_promises_part1/sketch.js
@@ -21,16 +21,12 @@ function setup(){
 
   // Getting data in JSON format
   fetch(wordnikAPI)
-    .then(response => {
-      return response.json();
-    })
+    .then(response => response.json())
     .then(json => {
         createP(json.word);
         return fetch(giphyAPI + json.word);
       })
-    .then(response => {
-      return response.json();
-    })
+    .then(response => response.json())
     .then(json => {
       createImg(json.data[0].images['fixed_height_small'].url);
     })
