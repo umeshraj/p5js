0c9859bc9e11ace3064865774858968c4b824f2e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-16 07:44:23 -0500

    Goal is to mimic setTimeout

--- 16p12_promises_part2/sketch.js
+++ 16p12_promises_part2/sketch.js
@@ -1,9 +1,11 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 function setup(){
-  createCanvas(400, 300);
+  noCanvas();
+  setTimeout(sayHello, 1000);
 }
 
-function draw(){
-  background(51);
+
+function sayHello(){
+  createP('Hello!'); 
 }
