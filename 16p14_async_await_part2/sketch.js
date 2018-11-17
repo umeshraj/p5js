2a0489855af81cc37e6021d6b3e0663608c7299a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-17 08:38:13 -0500

    Adding number of words to wordnik as a parameter

--- 16p14_async_await_part2/sketch.js
+++ 16p14_async_await_part2/sketch.js
@@ -1,16 +1,16 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
-const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=3&maxLength=5&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
+const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
 const giphyAPI = "https://api.giphy.com/v1/gifs/search?rating=PG&api_key=dc6zaTOxFJmzC&q=";
 
 
 function setup(){
   noCanvas();
-  wordGIF()
+  wordGIF(4)
     .then(results => {
       createP(results.word);
       createImg(results.img);
-      return wordGIF();
+      return wordGIF(5);
     })
     .then(results => {
       createP(results.word);
@@ -19,9 +19,9 @@ function setup(){
     .catch(err => console.error(err));
 }
 
-async function wordGIF(){
+async function wordGIF(num){
 
-    let response1 = await fetch(wordnikAPI);
+    let response1 = await fetch(wordnikAPI + '&minLength=' + num + '&maxLength=' + num);
     let json1 = await response1.json();
     let response2 = await fetch(giphyAPI + json1.word);
     let json2 = await response2.json();
