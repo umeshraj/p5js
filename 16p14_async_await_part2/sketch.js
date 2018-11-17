db41c46e7959fe74c31435c2f19b64146b93bd13
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-17 08:26:20 -0500

    Rewriting old GIF code with async/await

--- 16p14_async_await_part2/sketch.js
+++ 16p14_async_await_part2/sketch.js
@@ -1,15 +1,32 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
-const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=5&maxLength=-1&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
+const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=3&maxLength=5&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
 const giphyAPI = "https://api.giphy.com/v1/gifs/search?rating=PG&api_key=dc6zaTOxFJmzC&q=";
 
 
 function setup(){
   noCanvas();
-  wordGIF();
+  wordGIF()
+    .then(results => {
+      createP(results.word);
+      createImg(results.img);
+    })
+    .catch(err => console.error(err));
 }
 
-function wordGIF(){
+async function wordGIF(){
+
+    let response1 = await fetch(wordnikAPI);
+    let json1 = await response1.json();
+    let response2 = await fetch(giphyAPI + json1.word);
+    let json2 = await response2.json();
+    let img_url = json2.data[0].images['fixed_height_small'].url;
+
+    return{
+      word: json1.word,
+      img: img_url
+    }
+
     // Getting data in JSON format
     fetch(wordnikAPI)
     .then(response => response.json())
