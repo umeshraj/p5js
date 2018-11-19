cd22a298dd9e42540e05ddb5e864a7095441fe2b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-19 07:14:27 -0500

    Using preload to load sound

--- 17p91_load_play_sound/sketch.js
+++ 17p91_load_play_sound/sketch.js
@@ -1,7 +1,14 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
+var sound;
+
+function preload(){
+  sound  = loadSound('rainbow.mp3');
+}
+
 function setup(){
   createCanvas(400, 300);
+  sound.play();
 }
 
 function draw(){
