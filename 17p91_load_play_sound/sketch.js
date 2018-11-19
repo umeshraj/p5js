af47527f7e4844af12ebe2c3cd4a383a7350ad78
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-19 07:23:56 -0500

    Using callbacks

--- 17p91_load_play_sound/sketch.js
+++ 17p91_load_play_sound/sketch.js
@@ -3,19 +3,21 @@
 let song;
 let slider;
 
-function preload(){
-  song  = loadSound('rainbow.mp3');
-}
+// function preload(){
+//   song  = loadSound('rainbow.mp3');
+// }
 
 function setup(){
   createCanvas(400, 300);
   slider = createSlider(0, 1, 0.5, 0.1);
-   
+  song  = loadSound('rainbow.mp3', loaded);   
+}
+
+function loaded(){
   song.play();
 }
 
 function draw(){
   background(51);
   song.setVolume(slider.value());
-
 }
