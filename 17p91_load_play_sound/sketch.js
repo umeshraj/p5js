fe73cf6a71bc5e423131163495206acb30d3abce
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-19 07:37:22 -0500

    Pan left to right speaker

--- 17p91_load_play_sound/sketch.js
+++ 17p91_load_play_sound/sketch.js
@@ -13,7 +13,7 @@ function setup(){
   createCanvas(400, 300);
   sliderVolume = createSlider(0, 1, 0.5, 0.1);
   sliderRate = createSlider(0, 3, 0.5, 0.1);
-  sliderPan = createSlider(0, 1, 0.5, 0.1);
+  sliderPan = createSlider(-1, 1, 0., 0.1);
 
   song  = loadSound('rainbow.mp3', loaded);   
   song.setVolume(0.5);
@@ -26,6 +26,6 @@ function loaded(){
 function draw(){
   background(51);
   song.setVolume(sliderVolume.value());
-  song.rate(sliderRate.value());
-  // song.setPan(sliderVolume.value());
+  // song.rate(sliderRate.value());
+  song.pan(sliderPan.value());
 }
