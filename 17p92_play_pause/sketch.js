0d3e14750c3ebd2685d9276ef0ef273d3aa5b3b2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-19 07:44:50 -0500

    Play only when button is clicked.

--- 17p92_play_pause/sketch.js
+++ 17p92_play_pause/sketch.js
@@ -1,31 +1,25 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 let song;
-let sliderVolume;
-let sliderRate;
-let sliderPan;
+let button;
 
-// function preload(){
-//   song  = loadSound('rainbow.mp3');
-// }
 
 function setup(){
   createCanvas(400, 300);
-  sliderVolume = createSlider(0, 1, 0.5, 0.1);
-  sliderRate = createSlider(0, 3, 0.5, 0.1);
-  sliderPan = createSlider(-1, 1, 0., 0.1);
-
   song  = loadSound('rainbow.mp3', loaded);   
+  button = createButton("Play");
+  button.mousePressed(togglePlaying);
   song.setVolume(0.5);
 }
 
 function loaded(){
+  console.log('Loaded');
+}
+
+function togglePlaying(){
   song.play();
 }
 
 function draw(){
   background(51);
-  song.setVolume(sliderVolume.value());
-  // song.rate(sliderRate.value());
-  song.pan(sliderPan.value());
 }
