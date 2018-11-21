fab7b27b248a47e80c88c41225d5429d1b56f4a4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-21 08:09:34 -0500

    Changes to ADSR to make sound more piano-like

--- 17p7_adsr_envelope/sketch.js
+++ 17p7_adsr_envelope/sketch.js
@@ -10,8 +10,8 @@ function setup(){
 
   // set up adsr envelope
   env = new p5.Env();
-  env.setADSR(0.5, 0.25, 0.5, 0.1);
-  env.setRange(0.8, 0);
+  env.setADSR(0.05, 0.1, 0.5, 1);
+  env.setRange(1.2, 0);
 
   // set up the waveform
   wave = new p5.Oscillator();
