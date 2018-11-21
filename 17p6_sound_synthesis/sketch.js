02f976c190489fff7e66b47524f9d3b1e8f59064
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-21 07:45:07 -0500

    Play/pause button

--- 17p6_sound_synthesis/sketch.js
+++ 17p6_sound_synthesis/sketch.js
@@ -1,16 +1,34 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 let wave;
+let playing; 
 
 function setup(){
   createCanvas(400, 300);
   wave = new p5.Oscillator();
   wave.setType('sine');
-  wave.start();
-  wave.amp(1);
-  wave.freq(300);
+
+  button = createButton('play/pause');
+  button.mousePressed(toggle);
 }
 
 function draw(){
-  background(51);
+  if (playing){
+    background(255, 0, 255);
+  }else{
+    background(51);
+  }
+}
+
+function toggle(){
+  if (!playing){
+    wave.start();
+    wave.amp(1);
+    wave.freq(300);
+  
+    playing = true;
+  }else{
+    wave.stop();
+    playing = false;
+  }
 }
