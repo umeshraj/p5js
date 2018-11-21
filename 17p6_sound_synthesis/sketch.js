e653e78d96d3a0e4b01f2bb88a101f285b216b15
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-21 07:51:45 -0500

    Add slider to control frequency

--- 17p6_sound_synthesis/sketch.js
+++ 17p6_sound_synthesis/sketch.js
@@ -1,21 +1,25 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 let wave;
-let playing; 
+let playing = false; 
 
 function setup(){
   createCanvas(400, 300);
   wave = new p5.Oscillator();
   wave.setType('sine');
   wave.start()
-  wave.amp(1);
+  wave.amp(0);
   wave.freq(300);
 
   button = createButton('play/pause');
   button.mousePressed(toggle);
+
+  // add slider to control frequencies
+  slider = createSlider(100, 1200, 440);
 }
 
 function draw(){
+  wave.freq(slider.value());
   if (playing){
     background(255, 0, 255);
   }else{
