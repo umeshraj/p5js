859c1656c972a40377750d8a00d2e230b15de1f7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-22 07:02:56 -0500

    Map mic to ellipse radius

--- 17p8_microphone_input/sketch.js
+++ 17p8_microphone_input/sketch.js
@@ -1,9 +1,19 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
+let mic;
+
 function setup(){
   createCanvas(400, 300);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw(){
+  let vol = mic.getLevel();
+  console.log(vol);
+
   background(51);
+  let rad = map(vol, 0, 1, 10, 100);
+  fill(255);
+  ellipse(width/2, height/2, rad);
 }
