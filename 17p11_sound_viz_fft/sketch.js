4b493c5634d0a240bee8405b75e3e689e35003db
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-23 07:48:11 -0500

    Circular plot of fft

--- 17p11_sound_viz_fft/sketch.js
+++ 17p11_sound_viz_fft/sketch.js
@@ -4,8 +4,6 @@ let mic;
 let song;
 let button;
 let fft;  // fft object
-let volHistory = []; 
-let w; // width of fft bands
 
 
 function preload(){
@@ -19,8 +17,7 @@ function setup(){
   button = createButton('Toggle');
   button.mousePressed(toggleSong);
   // song.play();  
-  fft = new p5.FFT(0.9,64);
-  w = width/64;
+  fft = new p5.FFT(0.9,128);
 }
 
 function toggleSong(){
@@ -37,13 +34,30 @@ function draw(){
   let spectrum = fft.analyze();
   // console.log(spectrum.length);
   // plot the spectrum
-  noStroke();
-  for (let i=0; i<spectrum.length; i++){
-    let amp = spectrum[i];
-    let y = map(amp, 0, 255, height, 0);
-    // line(i*w, height, i*w, y);
-    fill(i, 255, 255);
-    rect(i*w, y, w-5, height-y);
-  }
+  // noStroke();
+  // for (let i=0; i<spectrum.length; i++){
+  //   let amp = spectrum[i];
+  //   let y = map(amp, 0, 255, height, 0);
+  //   // line(i*w, height, i*w, y);
+  //   fill(i, 255, 255);
+  //   rect(i*w, y, w-5, height-y);
+  // }
   
+  // plotting as a circle
+  noStroke()
+  translate(width/2, height/2);
+  // beginShape();
+  for (let i=0; i<spectrum.length; i++){
+    let angle = map(i, 0, spectrum.length, 0, 360);
+    let amp = spectrum[i];
+    let r = map(amp, 0, 256, 20, 100);
+    let x = r * cos(angle);
+    let y = r * sin(angle);
+    stroke(i, 255, 255);
+    line(0, 0, x, y);
+    // vertex(x, y);
+  }
+  // endShape();
+
+
 }
