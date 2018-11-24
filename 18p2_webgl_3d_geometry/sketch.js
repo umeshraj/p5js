98c58430d951c1a55ae6cf970b350a4c4b5df9df
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-24 07:43:47 -0500

    Torus

--- 18p2_webgl_3d_geometry/sketch.js
+++ 18p2_webgl_3d_geometry/sketch.js
@@ -14,13 +14,14 @@ function draw(){
   noStroke();
   fill(0, 0, 255);
   // translate(mouseX - width/2, mouseY-height/2);
-  translate(0, 0, mouseX);
+  // translate(0, 0, mouseX);
 
   rotateZ(angle * 0.2);
   rotateY(angle * 0.3);
   rotateX(angle * 0.1);
   // rect(0, 0, 150, 100);
-  box(10, 100, 50);
+  // box(10, 100, 50);
+  torus(100, 10);
 
   angle += 0.07;
 }
