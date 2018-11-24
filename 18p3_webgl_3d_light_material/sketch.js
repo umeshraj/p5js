f718e04c6c9404a81a1139ee5ef6b24579eb546f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-24 08:07:01 -0500

    Ambient white light and blue torus

--- 18p3_webgl_3d_light_material/sketch.js
+++ 18p3_webgl_3d_light_material/sketch.js
@@ -11,9 +11,10 @@ function draw(){
 
   rectMode(CENTER);
 
-  // noStroke();
   // fill(0, 0, 255);
-  normalMaterial();
+  // normalMaterial();
+  ambientLight(255);
+  ambientMaterial(0, 0, 255);
   // translate(mouseX - width/2, mouseY-height/2);
   // translate(0, 0, mouseX);
 
@@ -22,6 +23,7 @@ function draw(){
   rotateX(angle * 0.1);
   // rect(0, 0, 150, 100);
   // box(10, 100, 50);
+  noStroke();
   torus(100, 10);
 
   angle += 0.07;
