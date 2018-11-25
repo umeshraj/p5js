87d9c085b71013c7b5d919776df7ad5cb8b0f980
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-25 10:37:44 -0500

    Adding puppy texture to a plane 3d primitive

--- 18p4_webgl_texture/sketch.js
+++ 18p4_webgl_texture/sketch.js
@@ -1,6 +1,10 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 let angle = 0;
+let puppy;
+function preload(){
+  puppy = loadImage('images/puppy.jpeg');
+}
 
 function setup(){
   createCanvas(400, 300, WEBGL);
@@ -8,32 +12,16 @@ function setup(){
 
 function draw(){
   background(128);
-  let dx = mouseX - width/2;
-  let dy = mouseY - height/2;
-  let vec = createVector(dx, dy, 0);
-  vec.normalize();
+  ambientLight(255);
 
-  // rectMode(CENTER);
-
-  // fill(0, 0, 255);
-  // normalMaterial();
-  // ambientLight(255);
-  // pointLight(0, 0, 255, -200, 0, +200);
-  // pointLight(255, 0, 0, 0, +200, +200);
-  directionalLight(255, 255, 0, vec);
-
-  ambientMaterial(255);
-  // specularMaterial(255);
-  // translate(mouseX - width/2, mouseY-height/2);
-  // translate(0, 0, mouseX);
+  texture(puppy);
 
   rotateZ(angle * 0.2);
   rotateY(angle * 0.3);
   rotateX(angle * 0.1);
-  // rect(0, 0, 150, 100);
-  // box(10, 100, 50);
+
   noStroke();
-  torus(100, 10);
+  plane(200, 200);
 
   angle += 0.07;
 }
