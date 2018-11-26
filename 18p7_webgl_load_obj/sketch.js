0f89c743b88046e0be67f46c056e54bdd70d74d7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-26 08:23:12 -0500

    Base code for 18p7

---
+++ 18p7_webgl_load_obj/sketch.js
@@ -0,0 +1,25 @@
+/// <reference path="./p5d/p5.global-mode.d.ts" />
+
+let angle = 0;
+
+function preload(){
+  puppy = loadImage("images/puppy.jpeg");
+}
+
+function setup(){
+  createCanvas(400, 300, WEBGL);
+}
+
+function draw(){
+  background(0);
+  
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0, 0, -1);
+  rotateX(angle);
+  rotateY(angle * 1.3);
+  rotateZ(angle * 0.7);
+
+  box(100);
+
+  angle += 0.03;
+}
