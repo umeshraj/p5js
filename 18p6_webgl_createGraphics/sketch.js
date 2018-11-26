389d4367c563683399eec3c28ce1ef13a0515670
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-26 07:34:56 -0500

    Simple box rotating

--- 18p6_webgl_createGraphics/sketch.js
+++ 18p6_webgl_createGraphics/sketch.js
@@ -1,9 +1,19 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
+let angle = 0;
+
 function setup(){
-  createCanvas(400, 300);
+  createCanvas(400, 300, WEBGL);
 }
 
 function draw(){
-  background(51);
+  background(0);
+  ambientLight(100);
+  directionalLight(255, 255, 255, 1, -1, 0);
+  rotateX(angle);
+  rotateY(angle * 1.3);
+  rotateZ(angle * 0.7);
+  box(100);
+
+  angle += 0.03;
 }
