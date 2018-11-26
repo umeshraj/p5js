05cb4ba2849df14e22f563ed85dc74776303d382
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-26 07:41:01 -0500

    Put createGraphics on rotating box

--- 18p6_webgl_createGraphics/sketch.js
+++ 18p6_webgl_createGraphics/sketch.js
@@ -2,6 +2,7 @@
 
 let angle = 0;
 let puppy;
+let graphics;
 
 function preload(){
   puppy = loadImage("images/puppy.jpeg");
@@ -9,17 +10,20 @@ function preload(){
 
 function setup(){
   createCanvas(400, 300, WEBGL);
+  graphics = createGraphics(200, 200);
+  graphics.background(255);
 }
 
 function draw(){
   background(0);
+  graphics.background(random(255));
   ambientLight(100);
   directionalLight(255, 255, 255, 0, 0, -1);
   rotateX(angle);
   rotateY(angle * 1.3);
   rotateZ(angle * 0.7);
 
-  texture(puppy);
+  texture(graphics);
   box(100);
 
 
