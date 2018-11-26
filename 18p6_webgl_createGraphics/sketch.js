e39f4203088b59e10984bc8ed8d9e96c380ca76e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-26 07:39:04 -0500

    Texture of image on rotating box

--- 18p6_webgl_createGraphics/sketch.js
+++ 18p6_webgl_createGraphics/sketch.js
@@ -1,6 +1,11 @@
 /// <reference path="./p5d/p5.global-mode.d.ts" />
 
 let angle = 0;
+let puppy;
+
+function preload(){
+  puppy = loadImage("images/puppy.jpeg");
+}
 
 function setup(){
   createCanvas(400, 300, WEBGL);
@@ -9,11 +14,14 @@ function setup(){
 function draw(){
   background(0);
   ambientLight(100);
-  directionalLight(255, 255, 255, 1, -1, 0);
+  directionalLight(255, 255, 255, 0, 0, -1);
   rotateX(angle);
   rotateY(angle * 1.3);
   rotateZ(angle * 0.7);
+
+  texture(puppy);
   box(100);
+
 
   angle += 0.03;
 }
