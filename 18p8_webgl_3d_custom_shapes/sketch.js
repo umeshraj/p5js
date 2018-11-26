ade90828625d2768ec653224c5b77268cb78108f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-26 08:39:29 -0500

    Base code for 18p8

---
+++ 18p8_webgl_3d_custom_shapes/sketch.js
@@ -0,0 +1,25 @@
+/// <reference path="./p5d/p5.global-mode.d.ts" />
+
+let angle = 0;
+let puppy;
+
+function preload(){
+  puppy = loadImage("images/puppy.jpeg");
+}
+
+function setup(){
+  createCanvas(400, 300, WEBGL);
+}
+
+function draw(){
+  background(0);
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0, 0, -1);
+  rotateX(angle);
+  rotateY(angle * 1.3);
+  rotateZ(angle * 0.7);
+
+  texture(puppy);
+  box(100);
+  angle += 0.03;
+}
